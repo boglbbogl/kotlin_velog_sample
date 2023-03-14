62c351893ec154746267b694d73fd52338375344
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-03-14 15:42:30 +0900

    test app count

--- app/src/main/java/com/example/kotlinvelogsample/MainActivity.kt
+++ app/src/main/java/com/example/kotlinvelogsample/MainActivity.kt
@@ -2,16 +2,11 @@ package com.example.kotlinvelogsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val buttonEvent = findViewById<Button>(R.id.buttonEvent)
-        buttonEvent.setOnClickListener{
-            print("click")
-        }
     }
 }
